d14d7fbe7e6ef4fd9d34aad2136510d1919e2532
Neamtiu Zaris <zneamtiu@gmail.com>
2025-07-14 21:54:27 +0300

    Add editorconfig with tab size 2

--- phone_number_input/src/components/PhoneNumberInput/PhoneNumberInput.tsx
+++ phone_number_input/src/components/PhoneNumberInput/PhoneNumberInput.tsx
@@ -9,7 +9,8 @@ interface PhoneNumberInputProps {
 function PhoneNumberInput({onValidChange, label = "US Phone Number", placeholder = "(294) 917-8449"} : PhoneNumberInputProps) {
   return (
     <div className="PhoneNumberDiv">
-        <hr style={{ width: "100%", borderColor: "lightblue"}}/>
+			<hr style={{ width: "100%", borderColor: "lightblue"}}/>
+			
       <h1>Phone Number Input Component</h1>
       {/* Add your component logic here */}
     </div>
