1396f35a2c363333c477678fa401f4cc2f41b932
Neamtiu Zaris <zneamtiu@gmail.com>
2025-07-14 23:26:06 +0300

    Implemented functionality, begin testing

--- phone_number_input/src/App.tsx
+++ phone_number_input/src/App.tsx
@@ -3,15 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import PhoneNumberInput from './components/PhoneNumberInput';
 
-const onValidPhoneNumberChange = () => {}
+const onValidPhoneNumberChange = (phoneNumber: string) => {
+  console.log("PARENT: received valid phone number -- " + phoneNumber);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <PhoneNumberInput onValidChange={onValidPhoneNumberChange}/>
       </header>
-      <PhoneNumberInput onValidChange={onValidPhoneNumberChange}/>
+      
     </div>
   );
 }
--- phone_number_input/src/components/PhoneNumberInput/PhoneNumberInput.tsx
+++ phone_number_input/src/components/PhoneNumberInput/PhoneNumberInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './PhoneNumberInput.css';
 
 interface PhoneNumberInputProps {
@@ -6,13 +7,49 @@ interface PhoneNumberInputProps {
     placeholder?: string
 }
 
+const validatePhoneNumber = (phoneNumber: string): boolean => {
+	// Only digits and formatting characters allowed
+	for(const char of phoneNumber) {
+		if(!((char >= '0' && char <= '9') || char === ' ' || char === '-' || char === '(' || char === ')'))
+			return false;
+	}
+
+	phoneNumber = phoneNumber.replaceAll(' ', '');
+	phoneNumber = phoneNumber.replaceAll('-', '');
+	phoneNumber = phoneNumber.replaceAll('(', '');
+	phoneNumber = phoneNumber.replaceAll(')', '');
+
+	// And must be 10 digits
+	return phoneNumber.length === 10;
+}
+
+const formatPhoneNumber = (phoneNumber: string): string => {
+	// Remove spaces and '-'
+	phoneNumber = phoneNumber.replaceAll(' ', '');
+	phoneNumber = phoneNumber.replaceAll('-', '');
+	phoneNumber = phoneNumber.replaceAll('(', '');
+	phoneNumber = phoneNumber.replaceAll(')', '');
+
+	phoneNumber = '(' + phoneNumber.substring(0, 3) + ') ' + phoneNumber.substring(3, 6) + '-' + phoneNumber.substring(6);
+
+	return phoneNumber;
+}
+
 function PhoneNumberInput({onValidChange, label = "US Phone Number", placeholder = "(294) 917-8449"} : PhoneNumberInputProps) {
-  return (
-    <div className="PhoneNumberDiv">
-			<hr style={{ width: "100%", borderColor: "lightblue"}}/>
-			
-      <h1>Phone Number Input Component</h1>
-      {/* Add your component logic here */}
+  const [phoneNumber, setPhoneNumber] = useState('');
+	console.log("PHONE NUMBER: " + phoneNumber);
+
+	const isPhoneNumberCorrect = validatePhoneNumber(phoneNumber);
+	if(isPhoneNumberCorrect) {
+		onValidChange(formatPhoneNumber(phoneNumber));
+	}
+
+	return (
+    <div className='PhoneNumberDiv'>			
+			<label className='PhoneNumberLabel'>{label}</label>
+			<input className='PhoneNumberInput' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder={placeholder} 
+				style={{ borderColor: isPhoneNumberCorrect ? "aqua" : "red"}}></input>
+			{!isPhoneNumberCorrect && <label className='PhoneNumberLabel' style={{color: "red"}}>Phone number is incorrect!</label>}
     </div>
   );
 }
