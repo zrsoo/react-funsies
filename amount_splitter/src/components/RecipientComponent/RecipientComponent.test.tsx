029719c70f1fbbe87baa1c0bc02355797f77f178
Neamtiu Zaris <zneamtiu@gmail.com>
2025-07-15 21:34:31 +0300

    RecipientComponent tests

--- amount_splitter/src/components/RecipientComponent/RecipientComponent.test.tsx
+++ amount_splitter/src/components/RecipientComponent/RecipientComponent.test.tsx
@@ -1,7 +1,36 @@
-import { assert } from "console"
+import { assert, trace } from "console"
+import RecipientComponent from "./RecipientComponent";
+import { fireEvent, render, screen } from "@testing-library/react";
 
-describe('mock test', () => {
-    test('mock test', () => {
-        assert(true);
+describe('Integration testing for RecipientComponent', () => {
+    const mockUpdate = jest.fn();
+    const mockDelete = jest.fn();
+    const mockProps = {id: 1, name: "bobi", amount: 50, onUpdate: mockUpdate, onDelete: mockDelete}
+
+    test('onChange for name is fired', () => {
+        render(<RecipientComponent { ...mockProps }/>);
+        const input = screen.getByDisplayValue("bobi");
+
+        fireEvent.change(input, {target: {value: "asd"}});
+
+        expect(mockUpdate).toHaveBeenCalledWith(1, undefined, "asd");
+    });
+
+    test('onChange for amount is fired', () => {
+        render(<RecipientComponent { ...mockProps }/>);
+        const input = screen.getByDisplayValue("50");
+
+        fireEvent.change(input, {target: {value: "100"}});
+
+        expect(mockUpdate).toHaveBeenCalledWith(1, 100, undefined);
+    });
+
+    test('onClick for delete button', () => {
+        render(<RecipientComponent { ...mockProps }/>);
+        const button = screen.getByText("Delete");
+
+        fireEvent.click(button);
+
+        expect(mockDelete).toHaveBeenCalledWith(1);
     });
 });
